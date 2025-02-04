d5dd96d4ba189fb7ad0919eeb7a22f69271a6e44
cdadashovkhandan <chingizdkhandan@gmail.com>
2025-02-04 17:36:08 +0100

    add text box; enablement
    Co-authored-by: Miguel Bartelsman <mbartelsm@users.noreply.github.com>

--- src/extension.ts
+++ src/extension.ts
@@ -1,7 +1,6 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
+/// Append an input string and number to every single selection in a multi-line selection.
 function numberText(textPromptInput: string, editor: vscode.TextEditor) {
 	editor.edit(editBuilder => {
 		for (const [index, sel] of editor.selections.entries()) {
@@ -10,36 +9,27 @@ function numberText(textPromptInput: string, editor: vscode.TextEditor) {
 	});
 }
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
+/// Main extension activation
 export function activate(context: vscode.ExtensionContext) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
 	console.log('Extension Active');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
+	// Command: Complete Selection
 	const disposable = vscode.commands.registerCommand('multicurse.completeSelected', () => {
 
 		const editor = vscode.window.activeTextEditor;
 
 		if (editor) {
-			numberText("", editor);
+			var input = vscode.window.showInputBox();
+			
+			input.then(text => {
+				numberText(text || "", editor)
+			});
 		}
-
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Boopity Scoop');
-
-		// Show dummy text prompt
-
-		
 	});
 
 	context.subscriptions.push(disposable);
 }
 
-// This method is called when your extension is deactivated
+/// Deactivation
 export function deactivate() {}
