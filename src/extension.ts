418db1790352d2d94fab5b281000c607041d3aff
cdadashovkhandan <chingizdkhandan@gmail.com>
2025-02-04 17:20:22 +0100

    Basic number range appending functionality
    Co-authored-by: Miguel Bartelsman <mbartelsm@users.noreply.github.com>

--- src/extension.ts
+++ src/extension.ts
@@ -2,21 +2,40 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
+function numberText(textPromptInput: string, editor: vscode.TextEditor) {
+	editor.edit(editBuilder => {
+		for (const [index, sel] of editor.selections.entries()) {
+			editBuilder.replace(sel, textPromptInput + String(index));
+		}
+	});
+}
+
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "multicurse" is now active!');
+	console.log('Extension Active');
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('multicurse.helloWorld', () => {
+	const disposable = vscode.commands.registerCommand('multicurse.completeSelected', () => {
+
+		const editor = vscode.window.activeTextEditor;
+
+		if (editor) {
+			numberText("", editor);
+		}
+
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Multicurse!');
+		vscode.window.showInformationMessage('Boopity Scoop');
+
+		// Show dummy text prompt
+
+		
 	});
 
 	context.subscriptions.push(disposable);
